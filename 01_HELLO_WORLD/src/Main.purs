32bb978c17865df452903a7f212513a8c3962010
Code Whisperer <daniel@herald.to>
2022-05-12 07:09:55 -0400

    [initial]

---
+++ 01_HELLO_WORLD/src/Main.purs
@@ -0,0 +1,12 @@
+module Main where
+
+import Prelude (Unit)
+import Effect (Effect)
+import Effect.Console (log)
+
+message :: String
+message = "Hello world!"
+
+main ∷ Effect Unit
+main = do
+  log (message)
